9a7dfa9444223cd144580fdd679b88a24d39e54f
Raul <raulmayas20@gmail.com>
2023-05-03 12:37:47 -0600

    Creating routes for the models

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought, 
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thoughtController");
+
+// /api/ThoughtsgetThoughts
+router.route("/").get(getThoughts).post(createThought);
+
+// /api/ThoughtsgetThoughts/:videoId
+router
+  .route("/:videoId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/ThoughtsgetThoughts/:videoId/responses
+router.route("/:videoId/responses").post(addReaction);
+
+// /api/ThoughtsgetThoughts/:videoId/responses/:responseId
+router.route("/:videoId/responses/:responseId").delete(removeReaction);
+
+module.exports = router;
